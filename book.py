92c569df58433040cd6ce96e99f9e8625a0908ca
Ritik kumar <thakurritik38921@gmail.com>
2021-06-22 14:32:39 +0530

    initial commit

---
+++ book.py
@@ -0,0 +1,2 @@
+print('book is very essential for increasing the knowledge')
+print('it helps us to mantain the mental strength')
---
+++ name.py
@@ -0,0 +1,4 @@
+print('name = Ritik Kumar')
+print('roll no = 1902250100118')
+print('course = BTech')
+print('i am currently doing computer science engineering')
